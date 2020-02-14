243699591554bf053b54836a8a515b94ecd15a1d
noerm <noerm177@gmail.com>
2020-02-14 00:53:43 -0600

    Firt commit

---
+++ src/test/java/utility/Init.java
@@ -0,0 +1,31 @@
+package utility;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.apache.log4j.Logger.getLogger;
+
+
+public class Init extends DriverFactory {
+
+    private static final String URL = "http://the-internet.herokuapp.com/";
+
+    protected WebDriver driver = null;
+    private static final long IMPLICIT_TIME = 5;
+    private static Logger logger = getLogger(Init.class.getName());
+
+    /**web driver setup**/
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUP() {
+        logger.info("# Setup");
+        driver = getDriver();
+        driver.get(URL);
+        driver.manage().timeouts().implicitlyWait(IMPLICIT_TIME, SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    
+}
--- src/test/tests/first_try/firts_try.java
+++
@@ -1,2 +0,0 @@
-public class firts_try {
-}
